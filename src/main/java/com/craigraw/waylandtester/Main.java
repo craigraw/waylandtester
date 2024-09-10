51eaa3f3a1ea16393b1ba71276a773f12317a42f
Craig Raw <craigraw@gmail.com>
2024-09-10 14:52:14 +0200

    add app fxml

--- src/main/java/com/craigraw/waylandtester/Main.java
+++ src/main/java/com/craigraw/waylandtester/Main.java
@@ -3,6 +3,8 @@ package com.craigraw.waylandtester;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -11,23 +13,33 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
         Font.loadFont(Main.class.getResourceAsStream("/font/RobotoMono-Regular.ttf"), 13);
 
-        primaryStage.setTitle("Hello World!");
+        try {
+            FXMLLoader appLoader = new FXMLLoader(Main.class.getResource("app.fxml"));
+            Parent root = appLoader.load();
 
-        StackPane root = new StackPane();
-        for(int i = 0; i < 20000; i++) {
-            root.getChildren().add(createButton());
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(Main.class.getResource("app.css").toExternalForm());
+
+            stage.setTitle("Wayland Tester");
+            stage.setMinWidth(650);
+            stage.setMinHeight(708);
+            stage.setScene(scene);
+
+            stage.show();
+        } catch(IOException e) {
+            throw new IllegalStateException(e);
         }
-        primaryStage.setScene(new Scene(root, 300, 600));
-        primaryStage.show();
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "This is a modal dialog.");
         alert.showAndWait();
