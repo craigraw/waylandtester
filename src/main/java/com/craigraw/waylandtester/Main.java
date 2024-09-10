ab095bcee9ea9062a6d7fcd722d84352e4325948
Craig Raw <craigraw@gmail.com>
2024-09-10 14:16:23 +0200

    multiple buttons

--- src/main/java/com/craigraw/waylandtester/Main.java
+++ src/main/java/com/craigraw/waylandtester/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -20,22 +21,22 @@ public class Main extends Application {
         Font.loadFont(Main.class.getResourceAsStream("/font/RobotoMono-Regular.ttf"), 13);
 
         primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        VBox root = new VBox();
+        for(int i = 0; i < 20; i++) {
+            root.getChildren().add(createButton());
+        }
+        primaryStage.setScene(new Scene(root, 300, 600));
         primaryStage.show();
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "This is a modal dialog.");
         alert.showAndWait();
+    }
+
+    private static Button createButton() {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+        return btn;
     }
 }
