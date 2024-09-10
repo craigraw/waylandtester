8e6238a4ebd8701b6d730226a20efd5c84982860
Craig Raw <craigraw@gmail.com>
2024-09-10 14:09:47 +0200

    load a font

--- src/main/java/com/craigraw/waylandtester/Main.java
+++ src/main/java/com/craigraw/waylandtester/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +17,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Font.loadFont(Main.class.getResourceAsStream("/font/RobotoMono-Regular.ttf"), 13);
+
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
